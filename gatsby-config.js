4b7f6ced60f487db891b0ff9cc29c65e15718211
Andressa Melo <contato@andressamelo.com.br>
2019-09-25 20:39:55 -0300

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Andressa Melo`,
+    description: `I'm a Front-end developer. I've been creating functional projects, agile, with semantic codification, up-to-date and clean.`,
     author: `@gatsbyjs`,
   },
   plugins: [
