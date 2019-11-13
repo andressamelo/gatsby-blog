5a62c1650b18b0b6359e53055997536c89587daa
Andressa Melo <contato@andressamelo.com.br>
2019-11-13 10:12:08 -0300

    add recirects

--- src/components/Comments/index.js
+++ src/components/Comments/index.js
@@ -5,7 +5,7 @@ import ReactDisqusComments from "react-disqus-comments"
 import * as S from "./styled"
 
 const Comments = ({ url, title }) => {
-    const completeURL = `http://andressamelo.com${url}`
+    const completeURL = `https://andressamelo.com${url}`
 
     return(
         <S.CommentsWrapper>
